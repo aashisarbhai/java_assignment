074f6946652208941de3de0321b78693207df316
Aashi Sarbhai <166272665+aashisarbhai@users.noreply.github.com>
2024-07-12 09:18:35 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,8 @@
+//Enrollment Number:09350402025
+//Name:Aashi Sarbhai
+//QUESTION 1:Write a Java program to print welcome message on the screen? 
+class q1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programmimg");
+    }
+}
